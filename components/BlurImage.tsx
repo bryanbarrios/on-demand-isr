d0d2086867564b3ac58a2035d233cd5b2cc0a118
BRYAN STEVEN PEREZ BARRIOS <bryan.perez@est.uca.edu.ni>
2022-04-04 13:45:34 -0600

    refactor: clip image borders

--- components/BlurImage.tsx
+++ components/BlurImage.tsx
@@ -1,12 +1,13 @@
 import { FC, useState } from "react";
 import Image, { ImageProps } from "next/image";
 import { css } from "@emotion/react";
+import { Box } from "@chakra-ui/react";
 
 const BlurImage: FC<ImageProps> = ({ ...props }) => {
   const [isLoading, setIsLoading] = useState(true);
 
   const blurUpImage = css({
-    transition: ".6s ease-in-out all",
+    transition: ".6s ease-in-out",
     filter: isLoading ? "blur(40px) brightness(2)" : "blur(0) brightness(1)",
     transform: isLoading ? "scale(1.1)" : "scale(1.0)",
     willChange: "transform, filter",
@@ -14,11 +15,13 @@ const BlurImage: FC<ImageProps> = ({ ...props }) => {
   });
 
   return (
-    <Image
-      css={blurUpImage}
-      {...props}
-      onLoadingComplete={() => setIsLoading(false)}
-    />
+    <Box overflow="hidden" borderRadius="1.5rem">
+      <Image
+        css={blurUpImage}
+        {...props}
+        onLoadingComplete={() => setIsLoading(false)}
+      />
+    </Box>
   );
 };
 
